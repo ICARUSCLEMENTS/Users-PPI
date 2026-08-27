25b9ac22b41e9b485de7355f3e147c724694fd15
ICARUSCLEMENTS <icaroclemente0@gmail.com>
2026-08-27 15:07:47 -0300

    Atualizando algumas coisas

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Users.css";
 
-function User({ image, nome, idade, genero, email, telefone, empresa, cargo, cidade }) {
+function User({props}) {
   return (
     <div className="user-card">
       <img src={image} alt={nome} className="user-image" />
-      <h2>{nome}</h2>
-      <p>{idade} anos | {genero}</p>
-      <p><strong>Email:</strong> {email}</p>
-      <p><strong>Telefone:</strong> {telefone}</p>
-      <p><strong>Empresa:</strong> {empresa}</p>
-      <p><strong>Cargo:</strong> {cargo}</p>
-      <p><strong>Cidade:</strong> {cidade}</p>
+      <h2>{props.firstname} {props.lastname}</h2>
+      <p>{props.age} anos | {genero}</p>
+      <p><strong>Email:</strong> {props.email}</p>
+      <p><strong>Telefone:</strong> {props.phone}</p>
+      <p><strong>Empresa:</strong> {props.company.name}</p>
+      <p><strong>Cargo:</strong> {props.company.title}</p>
+      <p><strong>Cidade:</strong> {props.address.city} - {props.address.stateCode}</p>
     </div>
   );
 }
